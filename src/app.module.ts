35a4f604b21061d89288eeed3e699ca95026d9af
kacper-dabrowski <kdtw301@gmail.com>
2023-01-18 22:17:55 +0100

    feat(app): inject mongodb and config modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'mongodb',
+          url: configService.getOrThrow('DB_CONNECTION_STRING'),
+          entities: [User],
+          synchronize: true,
+          useUnifiedTopology: true,
+        };
+      },
+    }),
+    UsersModule,
+    AuthModule,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
