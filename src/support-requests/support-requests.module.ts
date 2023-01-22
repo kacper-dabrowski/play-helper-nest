7a6d6902d5c6b42826fdd9523a002b4398d10278
kacper-dabrowski <kdtw301@gmail.com>
2023-01-22 19:45:32 +0100

    feat(auth): add jwt authguard

--- src/support-requests/support-requests.module.ts
+++ src/support-requests/support-requests.module.ts
@@ -3,9 +3,10 @@ import { SupportRequestsService } from './support-requests.service';
 import { SupportRequestsController } from './support-requests.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SupportRequest } from './entities/support-request.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SupportRequest])],
+  imports: [TypeOrmModule.forFeature([SupportRequest]), AuthModule],
   controllers: [SupportRequestsController],
   providers: [SupportRequestsService],
 })
