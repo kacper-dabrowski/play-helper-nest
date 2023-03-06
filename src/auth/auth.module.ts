54c2ce3d8a2e38376ef80f165258450dfe44ea2c
kacper-dabrowski <kdtw301@gmail.com>
2023-03-06 21:58:11 +0100

    fix(auth): fix encoded and deleted user to pass authorization

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,13 +3,20 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../users/users.module';
+import { DefaultUserService } from '../users/users.service';
 import { AuthController } from './auth.controller';
 import { DefaultAuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
 
 @Module({
-  providers: [DefaultAuthService, LocalStrategy, JwtStrategy, ConfigService],
+  providers: [
+    DefaultAuthService,
+    LocalStrategy,
+    JwtStrategy,
+    ConfigService,
+    DefaultUserService,
+  ],
   controllers: [AuthController],
   imports: [
     UsersModule,
